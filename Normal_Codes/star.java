197ab1880621d38908240ecc2b55fcc5dafdfd85
takdirghonmode <takdirghonmode8@gmail.com>
2026-08-08 22:55:06 +0530

    The data star printing code is done

---
+++ Normal_Codes/star.java
@@ -0,0 +1,11 @@
+class Star{
+  public static void main(String arg[]){
+    int a=5;
+    for(int i=1;i<=a;i++){
+       for(int j=1;j<=i;j++){
+        System.out.print("*");
+       }
+       System.out.println(" ");
+    }
+  }
+}
