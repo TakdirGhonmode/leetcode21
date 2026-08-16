5ceded36fd0f97609efb52c4772f2e88ed5e27dc
takdirghonmode <takdirghonmode8@gmail.com>
2026-08-16 22:16:06 +0530

    the Randome nu code is done succeesfully

--- Normal_Codes/Randome.java
+++ Normal_Codes/Randome.java
@@ -1,3 +1,14 @@
+import java.util.Random;
 public class Randome {
-  
+  public static void main(String arg[]){
+    int n=1;
+    Random s=new Random();
+    int k=s.nextInt(100)+1;
+    System.out.println(k);
+    if(n==k){
+     System.out.println("sdfa");
+    }else{
+     System.out.print("ds");
+    }
+  }
 }
